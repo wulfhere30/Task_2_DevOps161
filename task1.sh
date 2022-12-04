e91b09ff2ca77847b92ec7baff97403987e406d9
Ruslan Kherson <wulfhere30@gmail.com>
2022-12-04 16:54:14 +0200

    Add two files

---
+++ task1.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+if [ "data1.txt" == "$1" ];
+  then echo -e "jobe\n/var/www/html/index.html\n/var/www/html/jobe"
+  else [ "data2.txt" == "$1" ];
+    echo -e "/bin/cat\n/var/www\n/var/www/html/index.html"
+fi
+#скріпт неправильний, але працює
---
+++ task2.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+echo -e "$1\n$2\n$3\n$4\n$5"
